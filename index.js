30b1e7ed03447120adb830af9733c654f3dde42a
javierbarbozam <barboza.javier497@gmail.com>
2023-07-28 15:55:21 -0600

    Add Reload Server modules

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
+const livereload = require("livereload");
+const connectLiveReload = require("connect-livereload");
 const router = require('./routes/db-routes');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 const app = express();
 
+app.use(connectLiveReload());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api/cards', router);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen('3000', () => {
   console.log('app listening on port 3000')
